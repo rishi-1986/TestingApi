d417dfb4162c56a82439bc462fabfa36233074b6
rishi-1986 <rishi.ranjan@saama.com>
2020-01-28 19:56:35 +0530

    Adding file for testing

---
+++ Bootstrap.java
@@ -0,0 +1,30 @@
+package com.saama.pdr.data.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class,
+		ElasticsearchAutoConfiguration.class })
+@ComponentScan(basePackages = { "com.saama" })
+
+public class Bootstrap extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootstrap.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Bootstrap.class);
+	}
+}
